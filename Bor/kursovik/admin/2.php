2fc3314f923a1800b9aeb8ae281be4308da86689
Boris199913 <46955168+Boris199913@users.noreply.github.com>
2019-01-23 11:10:12 +0300

    Add files via upload

---
+++ Bor/kursovik/admin/2.php
@@ -0,0 +1,13 @@
+<?php 
+echo "
+<br>
+<form>
+vруппа: 
+<input type=text  name=24 value=".$r2['group']."><br/>
+јдрес:
+<input type=text  name=25 value=".$r2['text']."><br/> 
+<input type=submit value='изменить' name=ok>
+</form>
+</div>";
+ 
+ ?>
